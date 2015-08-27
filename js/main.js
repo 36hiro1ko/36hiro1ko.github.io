a6d31e119aa3e291a43cca3da9fe378dd5d2f547
a36hiro1ko <starlilycat22272@nifty.com>
2015-08-27 03:50:36 +0000

    edit my profile

--- js/main.js
+++ js/main.js
@@ -1,42 +1,117 @@
 
 $(function(){
     
+   
+
     setTimeout('rect()'); //アニメーションを実行
+    setTimeout('grape()');
+    setTimeout('topimage()'); //アニメーションを実行
+    setTimeout('topimage2()'); //アニメーションを実行
+    setTimeout('topimage3()'); 
+    setTimeout('leaf1()');
+    
     
     
     /* snowfallを実行する */
-    $(document).snowfall({
-                /* shadow : true,*/
-                /* round : true, */
+    
+    $(document).snowfall({ 
                 minSize: 5,
-                maxSize: 20,
-                
+                maxSize: 20,              
                 image : [
 
-                      'http://lib.tree-web.net/image/snow_crystal_b/snow_crystal01.png',
-                      'http://lib.tree-web.net/image/snow_crystal_b/snow_crystal02.png',
-                      'http://lib.tree-web.net/image/snow_crystal_b/snow_crystal03.png',
-                      'http://lib.tree-web.net/image/snow_crystal_b/snow_crystal04.png',
-                      'http://lib.tree-web.net/image/snow_crystal_b/snow_crystal05.png',
-                      'http://lib.tree-web.net/image/snow_crystal_b/snow_crystal06.png',
-                      'http://lib.tree-web.net/image/snow_crystal_b/snow_crystal07.png',
-                      'http://lib.tree-web.net/image/snow_crystal_b/snow_crystal08.png',
-                      'http://lib.tree-web.net/image/snow_crystal_b/snow_crystal09.png'
+                      './img/sakura.png',
+                      './img/sakura2.png',
+                      './img/sakura3.png',
+                      './img/sakura4.png'
                 ]               
                 
-    })
+    }) 
     
+    
+
+     
 });
+
+/*
+$(function() {
+  $(window).scroll(function() {
+    var value = $(this).scrollTop();  //スクロール値を取得
+    $('#scrollValue').text(value);
+  });
+});
+*/
+
+
     
-    /* 風船に対してのアクション */
-    function rect() {
+/* 風船に対してのアクション */
+function rect() {
     $('#cat .inner').animate({
-        marginTop: '-=10px'
+        marginTop: '-=5px'
     }, 800).animate({
-        marginTop: '+=10px'
+        marginTop: '+=5px'
     }, 800);
         setTimeout('rect()', 1600); //アニメーションを繰り返す間隔
-    }
+}
+
+/* ヘッド画像(grape)に対してのアクション */
+function grape() {
+    $('#grape').animate({
+       backgroundPosition:'-=10px'
+    },1600).animate({
+      backgroundPosition:'+=10px'
+    },1600);
+        setTimeout('grape()', 1600); //アニメーションを繰り返す間隔
+}
+
+
+/* ヘッド画像に対してのアクション */
+function topimage() {
+    $('#topimage').animate({
+       backgroundPosition:'-=10px'
+    },1000).animate({
+      backgroundPosition:'+=10px'
+    },1000);
+        setTimeout('topimage()', 1600); //アニメーションを繰り返す間隔
+}
+
+/* ヘッド画像に対してのアクション */
+function topimage2() {
+    $('#topimage2').animate({
+       backgroundPosition:'-=10px'
+    },1500).animate({
+      backgroundPosition:'+=10px'
+    },1500);
+        setTimeout('topimage2()', 1600); //アニメーションを繰り返す間隔
+
+}
+
+/* ヘッド画像に対してのアクション */
+function topimage3() {
+    $('#topimage3').animate({
+       backgroundPosition:'-=10px'
+    },1300).animate({
+      backgroundPosition:'+=10px'
+    },1300);
+        setTimeout('topimage3()', 1600); //アニメーションを繰り返す間隔
+
+}
+
+/* sakura1に対してのアクション */
+function leaf1() {
+    $('#leaf1').animate({
+       backgroundPosition:'-=5px'
+    },1000).animate({
+      backgroundPosition:'+=5px'
+    },1000);
+        setTimeout('leaf1()', 1600); //アニメーションを繰り返す間隔
+
+}
+
+
+
+
+
+
 
     
     
